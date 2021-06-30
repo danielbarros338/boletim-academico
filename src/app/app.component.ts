fce2d7880212e85e3b1b9469539c7f1ffe7edbd1
danielbarros338 <danielbarros338@gmail.com>
2021-06-29 23:02:50 -0300

    Implements alert cookies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Component, OnInit } from '@angular/core';
 
 
@@ -6,5 +7,15 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: 'app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent{
+export class AppComponent implements OnInit{
+  
+  constructor(private snackBar: MatSnackBar){}
+
+  ngOnInit(): void{
+    this.snackBar.open(`Esse site utiliza cookies
+    para oferecer uma melhor experiência de navegação.
+    Clicando em OK você aceita nossa política de privacidade.`,'OK',{
+      verticalPosition:"top"
+    })
+  }
 }
