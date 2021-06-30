de34029c02d03a704bdcc9c7057201fe40d56724
danielbarros338 <danielbarros338@gmail.com>
2021-06-29 23:20:44 -0300

    Repair

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 
 @Component({
@@ -7,21 +6,5 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: 'app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  
-  constructor(private snackBar: MatSnackBar){}
-
-  ngOnInit(): void{
-    if(!document.cookie){
-      this._snackCall();
-    }
-  }
-
-  _snackCall(): void {
-    this.snackBar.open(`Esse site utiliza cookies
-    para oferecer uma melhor experiência de navegação.
-    Clicando em OK você aceita nossa política de privacidade.`,'OK',{
-      verticalPosition:"top"
-    })
-  }
+export class AppComponent{
 }
